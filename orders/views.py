e0edbfe78a965f435c9d8836198f787304e4c463
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2020-11-24 17:41:05 +0100

    fix the delete button

---
+++ orders/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from .models import OrderItem, Order
+from .forms import OrderForm
+from cart.cart import Cart
+from django.urls import reverse
+from django.shortcuts import redirect, render, get_object_or_404
+from .tasks import order_created
+
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.contrib.admin.views.decorators import staff_member_required
+import weasyprint
+
+
+# Create your views here.
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            # Iterate over each items and create an orderItem for each item
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+
+
+            # afterward successful order clear cart
+            cart.clear()
+            order_created.delay(order.id) 
+
+            # set order in session
+            request.session['order_id'] = order.id
+
+            # redirect to payment
+            return redirect(reverse('payment:process'))
+            # return render(request, 'orders/order/created.html', {'order': order})
+
+    else:
+        form = OrderForm
+    return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename= "order_{}.pdf"'.format(order.id)
+    weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')])
+    return response
---
+++ shop/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, get_object_or_404
+from shop.models import Category, Product
+from cart.forms import CartAddForm
+
+# Create your views here.
+
+
+def index(request):
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'shop/index.html', context)
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    cart_product_form = CartAddForm
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    context = {
+        'category': category,
+        'products': products,
+        'categories': categories,
+        'title': 'textiles',
+        'cart_product_form': cart_product_form
+    }
+
+    return render(request, 'shop/product/item_list.html', context)
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddForm
+    categories = Category.objects.all()
+    context = {
+        'product': product,
+        'title': product.category,
+        'cart_product_form': cart_product_form,
+        'categories': categories
+    }
+    return render(request, 'shop/product/item_detail.html', context)
