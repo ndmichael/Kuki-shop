97e057f4136d7b8798f93a1b4834d812d79cbfc9
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-02-17 23:19:31 +0100

    updated settings 2

--- kukishop/settings.py
+++ kukishop/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'd^hd%e*k937l-j8^pu@4ogx09@n=u^7gsj5=&%%)9e3rfr)ip-'
-SECRET_KEY=os.environ.get('SECRET_KEY_COOKELLA') 
+SECRET_KEY = 'd^hd%e*k937l-j8^pu@4ogx09@n=u^7gsj5=&%%)9e3rfr)ip-'
+# SECRET_KEY=os.environ.get('SECRET_KEY_COOKELLA') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG_VALUE') == 'True'
