4eb1f70e4213bf94f606cae7f8f33a5c21ec9d1d
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-02-17 23:51:04 +0100

    added django-heroku

--- kukishop/settings.py
+++ kukishop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -183,6 +184,8 @@ AWS_DEFAULT_UCL=None
 DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
 
 
 #django-allauth registraion settings 
