2be45eec111d4c8e754485babb81b76793d1c4ae
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-02-18 00:22:32 +0100

    updated secret key

--- kukishop/settings.py
+++ kukishop/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'd^hd%e*k937l-j8^pu@4ogx09@n=u^7gsj5=&%%)9e3rfr)ip-'
-SECRET_KEY=os.environ.get('SECRET_KEY_COOKELLA') 
+SECRET_KEY=os.environ.get('SECRET_KEY_KUKISHOP') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_VALUE') == 'True'
