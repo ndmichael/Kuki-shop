fbee4c26e4929776e3369798ab31acdb25506c95
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-03-01 15:21:38 +0100

    updated staticfile

--- kukishop/settings.py
+++ kukishop/settings.py
@@ -154,9 +154,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
