a4aa677a6ffd5590ae3b99fe0bd987ddc599e541
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-06-06 12:41:41 +0100

    fixed the view

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from orders.views import user_orders
 
 # Create your views here.
+
+
+@login_required
+def dashboard(request):
+    orders =  user_orders(request)
+    context = {
+        'orders': orders
+    }
+    return render(request, 'account/user/dashboard.html', context)
